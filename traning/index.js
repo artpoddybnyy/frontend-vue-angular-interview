2fbf0de55b8e799c364d03997128cd7f478b0489
Artrem Poddybnyy <artpoddybnyy@gmail.com>
2018-06-18 10:52:08 +0000

    added ching longest_palindrome

--- traning/index.js
+++ traning/index.js
@@ -188,6 +188,36 @@ grizzly.grow();
     }
     eachRecursive(obj)
 
+/** finde longest_palindrome in string */
+ function is_Palindrome(str1) {
+        let rev = str1.split("").reverse().join("");
+        return str1 == rev;
+    }
+
+    function longest_palindrome(str1) {
+
+        let max_length = 0;
+        let maxp = '';
+
+        for (let i = 0; i < str1.length; i++) {
+            let subs = str1.substr(i, str1.length);
+
+            for (let j = subs.length; j >= 0; j--) {
+                let sub_subs_str = subs.substr(0, j);
+                if (sub_subs_str.length <= 1)
+                    continue;
+
+                if (is_Palindrome(sub_subs_str)) {
+                    if (sub_subs_str.length > max_length) {
+                        max_length = sub_subs_str.length;
+                        maxp = sub_subs_str;
+                    }
+                }
+            }
+        }
+        return maxp;
+    }
+
 
 /** собеседование
  1 . имеет ли дж доступ к файловой системе, модно ли взять файл и че то с ним сделать ?
